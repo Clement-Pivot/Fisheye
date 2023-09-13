f3a9d076af85421408262a5f32b5320fec262a4f
Clement Pivot <8830707+malfeitor@users.noreply.github.com>
2023-09-13 18:26:51 +0200

    feat(perso): filter show chevron the right way

--- scripts/utils/filterButton.js
+++ scripts/utils/filterButton.js
@@ -19,29 +19,64 @@ export class FilterButton {
 
   expand () {
     this._filters.forEach(element => {
-      console.log(element)
       element.style.display = 'inline-flex'
     })
+    this._container.classList.add('active')
     this._expanded = true
+  }
+
+  showExpandedChevron () {
+    this._container.querySelector('.fa-chevron-up').classList.remove('fa-chevron-up')
+    this._filters.values().next().value.querySelector('i')
+      .classList.add('fa-chevron-down')
+  }
+
+  showSelectedChevron (order) {
+    // foreach because can be empty
+    this._container.querySelectorAll('.fa-chevron-down')
+      .forEach(icon => icon.classList.remove('fa-chevron-down'))
+    this._filters.forEach((filter, key) => {
+      if (key !== order) {
+        filter.style.display = 'none'
+      } else {
+        filter.querySelector('i').classList.add('fa-chevron-up')
+      }
+    })
   }
 
   filter (order) {
     if (!this._expanded) {
-      console.log('expand !')
+      this.showExpandedChevron()
       this.expand()
     } else {
-      console.log(order)
-      this._filters.forEach((filter, key) => {
-        if (key !== order) {
-          console.log(`cache ${key}`)
-          filter.style.display = 'none'
-        }
-      })
+      switch (order) {
+        case 'popularity':
+          this.filterByPopularity()
+          break
+        case 'date':
+          this.filterByDate()
+          break
+        case 'title':
+          this.filterByTitle()
+          break
+        default:
+          throw new Error('Unknow order')
+      }
+      this.showSelectedChevron(order)
+      this._container.classList.remove('active')
       this._expanded = false
     }
   }
 
   filterByPopularity () {
+    return 0
+  }
+
+  filterByDate () {
+    return 0
+  }
+
+  filterByTitle () {
     return 0
   }
 }
