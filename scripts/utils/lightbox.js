4649cd62f3f4ccda119f8ac88c16f60c62265649
Clement Pivot <8830707+malfeitor@users.noreply.github.com>
2023-09-14 11:54:08 +0200

    feat(lightbox): add video content and controls

--- scripts/utils/lightbox.js
+++ scripts/utils/lightbox.js
@@ -5,11 +5,25 @@ export class Lightbox {
   }
 
   show (media) {
-    console.log(media)
     this._container.style.display = 'flex'
-    const content = document.createElement('img')
-    content.setAttribute('src', media.image)
-
+    console.log(media)
+    // reset lightbox content
+    while (this._content.firstChild) {
+      this._content.removeChild(this._content.firstChild)
+    }
+    let content = null
+    if ('image' in media) {
+      content = document.createElement('img')
+      content.setAttribute('src', media.image)
+    } else if ('video' in media) {
+      content = document.createElement('video')
+      content.setAttribute('controls', true)
+      const source = document.createElement('source')
+      source.setAttribute('src', media.video)
+      content.appendChild(source)
+    } else {
+      throw new Error('Unknown media type')
+    }
     const title = document.createElement('p')
     title.classList.add('lightbox__title')
     title.textContent = media.title
