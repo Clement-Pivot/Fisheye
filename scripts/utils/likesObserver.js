8bac2e6329731583a3ea636c2fb463ada2b37fc1
Clement Pivot <8830707+malfeitor@users.noreply.github.com>
2023-10-05 18:36:31 +0200

    feat(likes): create observer

---
+++ scripts/utils/likesObserver.js
@@ -0,0 +1,46 @@
+export class LikesObserver {
+  constructor (medias, infosWrapper) {
+    this._medias = medias
+    this._infosWrapper = infosWrapper
+    this._totalLikes = 0
+  }
+
+  increment (media) {
+    this._totalLikes++
+    // change only text not childs
+    const text = media._likeContainer.childNodes[0]
+    text.textContent = Number(text.textContent) + 1
+    document.querySelector('.photograph-infos__likes').childNodes[0].textContent = this._totalLikes
+    media._likeContainer.parentNode.querySelector('.focusable').focus()
+    media.likeUnsubscribe(this)
+  }
+
+  subscribed (media) {
+    media._likeContainer.addEventListener('click', e => media.likeFire(e))
+    media._likeContainer.addEventListener('keydown', e => {
+      if (e.key === ' ' || e.key === 'Enter') {
+        media.likeFire(e)
+        e.preventDefault()
+      }
+    })
+    media._likeContainer.classList.add('focusable')
+    media._likeContainer.setAttribute('tabindex', 0)
+  }
+
+  unsubscribed (media) {
+    media._likeContainer.classList.remove('focusable')
+    media._likeContainer.setAttribute('tabindex', -1)
+  }
+
+  init () {
+    const infosLikes = this._infosWrapper.querySelector('.photograph-infos__likes')
+    this._medias.forEach(media => {
+      this._totalLikes += media.likes
+    })
+    infosLikes.textContent = this._totalLikes
+    const heart = document.createElement('i')
+    heart.classList.add('fa-solid')
+    heart.classList.add('fa-heart')
+    infosLikes.appendChild(heart)
+  }
+}
